c2e7131a78368644839f8b327087806278fcdd7c
Dennis Garcia <BlueBoxDev@outlook.com>
2022-09-11 20:09:23 -0500

    added some data calls

---
+++ src/infrastructure/services/data-service.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const baseURL = process.env.REACT_APP_NOT_API_URL;
+
+const axiosConfig = {
+  baseURL,
+};
+
+const dataService = {
+  login: (data) => {
+    return axios.post("api/account/login", data, axiosConfig);
+  },
+  getWeatherForcast: () => {
+    return axios.get("WeatherForecast", axiosConfig);
+  },
+};
+
+export default dataService;
