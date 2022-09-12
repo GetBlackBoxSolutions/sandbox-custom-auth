b2dbe8b08dcd12b61570be823e4d2b5e2d51fe1d
Dennis Garcia <BlueBoxDev@outlook.com>
2022-09-11 20:15:00 -0500

    setup login and route to dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { useRootStore } from "../../infrastructure/hooks/useRootStoreContext";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { currentUserStore } = useRootStore();
+
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <h4>Welcome {currentUserStore.displayName}</h4>
+    </div>
+  );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRootStore } from "../../infrastructure/hooks/useRootStoreContext";
+import dataService from "../../infrastructure/services/data-service";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErroMessage] = useState("");
+
+  const { currentUserStore } = useRootStore();
+  const navigate = useNavigate();
+
+  const onUserNameValueChanged = ({ target }) => {
+    setUserName(target.value);
+  };
+
+  const onPasswordValueChanged = ({ target }) => {
+    setPassword(target.value);
+  };
+
+  const onLoginClicked = async () => {
+    setErroMessage("");
+    if (userName.length === 0 || password.length === 0) {
+      setErroMessage("User name and/or password are missing");
+      return;
+    }
+    try {
+      const { data } = await dataService.login({ email: userName, password });
+      currentUserStore.setCurrentUser(
+        data.displayName,
+        data.userName,
+        data.token
+      );
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error);
+      if (error.response) {
+        const { data } = error.response;
+        console.log(data);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <h4>Login</h4>
+      <div>
+        <input type="text" value={userName} onChange={onUserNameValueChanged} />
+      </div>
+      <div>
+        <input
+          type="password"
+          value={password}
+          onChange={onPasswordValueChanged}
+        />
+      </div>
+      <div>
+        <button onClick={onLoginClicked}>Login</button>
+      </div>
+      <div>{errorMessage}</div>
+    </div>
+  );
 }
