12b80029b9c0b6f018606c73b9305f08bcef901f
Dennis Garcia <BlueBoxDev@outlook.com>
2022-09-26 19:40:35 -0500

    Added sass files and package

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import "./assets/styles/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import rootContextStore from "./infrastructure/stores/rootContextStore";
 import { RootStoreContext } from "./infrastructure/hooks/useRootStoreContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <RootStoreContext.Provider value={rootContextStore}>
