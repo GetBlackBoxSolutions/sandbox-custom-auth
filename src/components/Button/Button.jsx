54cd0bf8781305b9e2047ec37f226ad896c2018b
Dennis Garcia <BlueBoxDev@outlook.com>
2022-11-09 20:39:18 -0600

    Added more default style for buttons

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./Button.scss";
 
 export default function Button({ onClick, text, type = "default" }) {
-  const styleType = `btn-${type}`;
+  const styleType = `btn--${type}`;
   return (
     <button
       type="button"
       onClick={onClick}
       className={["btn", styleType].join(" ")}
     >
-      Login
+      {text}
     </button>
   );
 }
