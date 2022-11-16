58ff3667e5d981d81e9e573a29620f8140b92447
Dennis Garcia <BlueBoxDev@outlook.com>
2022-11-15 22:00:08 -0600

    Added endpoint to get current user

--- src/infrastructure/services/data-service.js
+++ src/infrastructure/services/data-service.js
@@ -5,7 +5,22 @@ const axiosConfig = {
   baseURL,
 };
 
+function protectedAxiosConfig(token) {
+  return {
+    withCredentials: true,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+}
+
 const dataService = {
+  currentUser: (token) => {
+    return axios.get("api/account/currentUser", {
+      ...axiosConfig,
+      ...protectedAxiosConfig(token),
+    });
+  },
   login: (data) => {
     return axios.post("api/account/login", data, axiosConfig);
   },
