de9670372f6f769ea12284a5d957c1b6097bbe95
Dennis Garcia <BlueBoxDev@outlook.com>
2022-12-05 19:59:21 -0600

    Added header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,22 @@ import "./Header.scss";
 export default function Header() {
   return (
     <header>
-      <nav>Navigaton</nav>
+      <nav>
+        <div>
+          <h3>Logo</h3>
+        </div>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Profile</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
