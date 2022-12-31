8260ffb40b0536a8c5d56237a04b2b4d118b64b2
Dennis Garcia <BlueBoxDev@outlook.com>
2022-12-31 13:07:34 -0600

    Update to form submission and added overlay

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,12 +6,17 @@ import dataService from "../../infrastructure/services/data-service";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import InputText from "../../components/Input/InputText";
 import Button from "../../components/Button/Button";
+import Spinner from "../../components/Spinner/Spinner";
+import withOverlay from "../../components/withOverlay/withOverlay";
+
+const SpinnerOverlay = () => withOverlay(<Spinner size="large" />);
 
 export default function Login() {
   const { currentUserStore, tokenStore } = useRootStore();
   const [userNameOrEmail, setUserNameOrEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErroMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
@@ -23,25 +28,28 @@ export default function Login() {
     setPassword(target.value);
   };
 
-  const onLoginClicked = async () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
     setErroMessage("");
     if (userNameOrEmail.length === 0 || password.length === 0) {
       setErroMessage("User name and/or password are missing");
       return;
     }
     try {
+      setLoading(true);
       const { data } = await dataService.login({
         userNameOrEmail,
         password,
       });
 
-      console.log(data);
-
       currentUserStore.setCurrentUser(data.userProfile);
       tokenStore.setAccessToken(data.token);
 
       navigate("/dashboard");
+      setLoading(false);
     } catch (error) {
+      setLoading(false);
       console.log(error);
       if (error.response) {
         const { data } = error.response;
@@ -56,19 +64,22 @@ export default function Login() {
     <div id="login" className="container">
       <h3>Sandbox</h3>
       <hr />
-      <InputText
-        placeholder="Username or email address"
-        value={userNameOrEmail}
-        onChange={onUserNameOrEmailValueChanged}
-      />
-      <InputText
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={onPasswordValueChanged}
-      />
-      <Button text="Sign in" onClick={onLoginClicked} />
-      {errorMessage && <ErrorMessage message={errorMessage} />}
+      <form onSubmit={handleSubmit}>
+        <InputText
+          placeholder="Username or email address"
+          value={userNameOrEmail}
+          onChange={onUserNameOrEmailValueChanged}
+        />
+        <InputText
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={onPasswordValueChanged}
+        />
+        <Button btnType="submit">Sign in</Button>
+        {errorMessage && <ErrorMessage message={errorMessage} />}
+      </form>
+      {loading && <SpinnerOverlay />}
     </div>
   );
 }
